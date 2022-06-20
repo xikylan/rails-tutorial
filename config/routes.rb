b1260779b73533644354506a7c5d7d5cb1b2b174
xikylan <lantheman8888@gmail.com>
2022-06-20 18:53:57 -0500

    stopped at 7.3.3

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root "articles#index"
 
   # Defines the root path route ("/")
-  # root "articles#index"
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+  resources :articles
 end
+
+
